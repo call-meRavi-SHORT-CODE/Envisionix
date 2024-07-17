0c9cd9bae0f017cbe64aac830944799b7b36b99b
call-meRavi-SHORT-CODE <ravikrishnaj25@gmail.com>
2024-07-17 12:36:24 +0530

    first commit

---
+++ app/(root)/transformation/add/[type]/page.tsx
@@ -0,0 +1,11 @@
+import React from 'react'
+
+const AddTransformationTypePage = () => {
+  return (
+    <div>
+      AddTransformation
+    </div>
+  )
+}
+
+export default AddTransformationTypePage
